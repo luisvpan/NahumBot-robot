71805d0df8266e9d2eef601893e259e182e29814
luisvpan <161976502+luisvpan@users.noreply.github.com>
2025-04-07 17:30:49 -0400

    Update brushless2.py

--- src/motor/brushless2.py
+++ src/motor/brushless2.py
@@ -1,27 +1,18 @@
 from gpiozero import AngularServo
-from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
-
-# Use the pigpio pin factory
-factory = PiGPIOFactory()
 
 # Configuración del pin GPIO donde se conectará el ESC
 ESC_PIN = 18
-servo = AngularServo(ESC_PIN, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory)
+servo = AngularServo(ESC_PIN, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 try:
     while True:
         servo.angle = 0
-        sleep(2)
+        sleep(3)
 
         servo.angle = 90
-        sleep(2)
+        sleep(3)
 
-        servo.angle = 0
-        sleep(2)
-
-        servo.angle = -90
-        sleep(2)
 
 except KeyboardInterrupt:
     print("Programa detenido")
