3d8ec6bf79e74fadd3a9bcbf9b8da8dd6e61395b
luisvpan <161976502+luisvpan@users.noreply.github.com>
2025-04-07 17:20:43 -0400

    Update brushless2.py

--- src/motor/brushless2.py
+++ src/motor/brushless2.py
@@ -1,30 +1,24 @@
-from gpiozero import PWMOutputDevice
+from gpiozero import AngularServo
 from time import sleep
 
 # Configuración del pin GPIO donde se conectará el ESC
 ESC_PIN = 18
-esc = PWMOutputDevice(ESC_PIN, frequency=50)
-
-def set_esc_speed(speed):
-    print(esc)
-    # Convertir el rango de velocidad de 0-100% a 0-180 (similar al Arduino)
-    duty_cycle = speed / 100  # Escala a un valor entre 0.0 y 1.0
-    esc.value = duty_cycle
-
-def read_potentiometer():
-    # Aquí deberías leer el valor analógico del potenciómetro
-    # Este código es genérico, necesitarás una biblioteca como `gpiozero` y un ADC (convertidor analógico-digital) para leer el potenciómetro.
-    pot_value = 1023  # Suponiendo un valor fijo como ejemplo
-    return pot_value
+servo = AngularServo(ESC_PIN, min_pulse=0.0006, max_pulse=0.0023)
 
 try:
     while True:
-        pot_value = read_potentiometer()  # Leer el valor del potenciómetro
-        scaled_value = (pot_value / 1023) * 100
-        print(scaled_value,"%")  # Escalar el valor a un rango de 0-100%
-        set_esc_speed(scaled_value)  # Configurar la velocidad del ESC
-        sleep(0.1)
+     
+        servo.angle = 0
+        sleep(2)
+  
+        servo.angle = 90
+        sleep(2)
+
+        servo.angle = 0
+        sleep(2)
+
+        servo.angle = -90
+        sleep(2)
 
 except KeyboardInterrupt:
     print("Programa detenido")
-    esc.value = 0  # Apagar el ESC
