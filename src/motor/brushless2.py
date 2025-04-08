b41d0a40c82b330a94130df76c2b012e627289a9
luisvpan <161976502+luisvpan@users.noreply.github.com>
2025-04-08 14:03:36 -0400

    Update brushless2.py

--- src/motor/brushless2.py
+++ src/motor/brushless2.py
@@ -2,7 +2,7 @@ from gpiozero import AngularServo
 from time import sleep
 from gpiozero.pins.pigpio import PiGPIOFactory
 
-factory = PiGPIOFactory()
+factory = PiGPIOFactory(host='localhost', port=8888)
 # Configuración del pin GPIO donde se conectará el ESC
 ESC_PIN = 18
 servo = AngularServo(ESC_PIN, min_pulse_width=0.0006, max_pulse_width=0.0023, pin_factory=factory)
@@ -11,5 +11,5 @@ for i in range(-90, 90):
     print(i)
     servo.angle = i
     sleep(0.5)
-    
+
 servo.angle = 42
