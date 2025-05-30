a4ad7210c96e25ca4a6c28429162a1a0041b72eb
luisvpan <161976502+luisvpan@users.noreply.github.com>
2025-05-30 15:36:12 -0400

    Update arduino_gps.py

--- src/gps/arduino_gps.py
+++ src/gps/arduino_gps.py
@@ -12,38 +12,17 @@ def read_gps_from_serial(port="/dev/ttyACM0", baudrate=115200):
             'speed': 0.0
         }
         print("Entro en read GPS from Serial")
-        while True:
-            try:
-                print("antes de line")
-                line = ser.readline().decode('ascii', errors='replace').strip()
-                if not line:
-                    continue
+
+        try:
+            print("antes de line")
+            line = ser.readline().decode('ascii', errors='replace').strip()
+            if line:
                 print("antes de msg")
                 msg = json.loads(line)
-                print("despeus de msg")
+                print("después de msg")
+
                 location = {
                     'lat': msg.get('lat', last_known_location['lat']),
                     'lng': msg.get('lng', last_known_location['lng']),
                     'orientation': msg.get('orientation', last_known_location['orientation']),
                     'speed': msg.get('speed', last_known_location['speed'])
-                }
-
-                last_known_location = location
-                print(location)
-
-            except json.JSONDecodeError:
-                continue
-            except Exception as e:
-                print(f"Error al leer datos: {e}")
-
-            time.sleep(0.1)  # Pequeño retraso para evitar uso excesivo de CPU
-
-    except serial.SerialException as e:
-        print(f"No se pudo abrir el puerto serial: {e}")
-    except KeyboardInterrupt:
-        print("Programa interrumpido por el usuario. Cerrando...")
-    finally:
-        if 'ser' in locals() and ser.is_open:
-            ser.close()  # Asegúrate de cerrar el puerto serial si está abierto
-        print("Puerto serial cerrado.")
-
