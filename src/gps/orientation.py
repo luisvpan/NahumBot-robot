852ab719ebc564a7cb929dc6b05156d25b842145
luisvpan <161976502+luisvpan@users.noreply.github.com>
2025-06-04 21:39:52 -0400

    Update orientation.py

--- src/gps/orientation.py
+++ src/gps/orientation.py
@@ -10,13 +10,15 @@ MODE = 0x02
 X_MSB = 0x03
 
 def read_orientation():
+    print("antes de smbus2")
     with smbus2.SMBus(1) as bus:
         # Configurar el sensor (modo continuo, ganancia, tasa de muestreo)
         bus.write_byte_data(HMC5883L_ADDRESS, CONFIG_A, 0x70)  # 8-average, 15 Hz default, normal measurement
         bus.write_byte_data(HMC5883L_ADDRESS, CONFIG_B, 0xA0)  # Gain = 5
         bus.write_byte_data(HMC5883L_ADDRESS, MODE, 0x00)      # Continuous measurement mode
-
+        print("bus =>", bus)
         # Leer 6 bytes desde el registro X_MSB
+        print("data =>", data)
         data = bus.read_i2c_block_data(HMC5883L_ADDRESS, X_MSB, 6)
 
     # Convertir bytes a valores 16-bit con signo
@@ -30,6 +32,8 @@ def read_orientation():
         y -= 65536
     if z > 32767:
         z -= 65536
+
+    print("x", x,  " y", y, " z", z)
 
     # Calcular heading en radianes y luego a grados
     heading_rad = math.atan2(y, x)
